1eead604db63076cb74c7037dc6e8563a154d394
nuuuwan <nuuuwan@gmail.com>
2024-03-09 20:08:12 +0530

    lint only

--- src/view/atoms/PlantPhotoMarker.js
+++ src/view/atoms/PlantPhotoMarker.js
@@ -16,22 +16,16 @@ export default function PlantPhotoMarker({
     : STYLE.PLANT_PHOTO.MARKER.CIRCLE;
 
   return (
-
-      <CircleMarker
-        center={plantPhoto.latlng}
-        radius={15}
-        key={`circle-${plantPhoto.id}`}
-        pathOptions={Object.assign({ fillColor: color }, styleCircle)}
-        eventHandlers={{
-          click: onClickInner,
-        }}
-        
-      >
-          <Tooltip opacity={0.9} >
-            {plantPhoto.scientificName}
-          </Tooltip>
-        </CircleMarker>
-
- 
+    <CircleMarker
+      center={plantPhoto.latlng}
+      radius={15}
+      key={`circle-${plantPhoto.id}`}
+      pathOptions={Object.assign({ fillColor: color }, styleCircle)}
+      eventHandlers={{
+        click: onClickInner,
+      }}
+    >
+      <Tooltip opacity={0.9}>{plantPhoto.scientificName}</Tooltip>
+    </CircleMarker>
   );
 }
