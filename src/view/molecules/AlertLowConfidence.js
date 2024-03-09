ccb1c0c1c2b33d6cf6b53c019ffcbe5d3d3c4459
nuuuwan <nuuuwan@gmail.com>
2024-03-09 20:39:00 +0530

    <Alert severity="error" sx={{ m: 2, p: 1, fontSize: "70%" }}>

--- src/view/molecules/AlertLowConfidence.js
+++ src/view/molecules/AlertLowConfidence.js
@@ -5,7 +5,7 @@ export default function AlertLowConfidence({ plantPhoto }) {
     return null;
   }
   return (
-    <Alert severity="error" sx={{ m: 3, p: 1, fontSize: "80%" }}>
+    <Alert severity="error" sx={{ m: 2, p: 1, fontSize: "70%" }}>
       <strong>Identification Confidence for this Photo is too low.</strong>
       <br />
       {plantPhoto.confidenceStrAll}
