986eb7ec95305ddc880b5c74c352b6c54693f069
nuuuwan <nuuuwan@gmail.com>
2024-03-09 15:01:47 +0530

    lint

--- src/view/molecules/ConfidenceView.js
+++ src/view/molecules/ConfidenceView.js
@@ -1,4 +1,4 @@
-import { Box, Typography } from "@mui/material";
+import {  Typography } from "@mui/material";
 import { WikiLink } from "../atoms";
 
 function ConfidenceViewItem({ plantResult }) {
