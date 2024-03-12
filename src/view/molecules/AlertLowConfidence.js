e9c791c46f34cbfd8fccdc0cbdec221d130220ab
nuuuwan <nuuuwan@gmail.com>
2024-03-12 11:39:09 +0530

    style

--- src/view/molecules/AlertLowConfidence.js
+++ src/view/molecules/AlertLowConfidence.js
@@ -5,11 +5,12 @@ import { Format } from "../../nonview/base";
 export default function AlertLowConfidence({ plantNetResult }) {
   const pLimit = Format.percent(PlantNetResult.LIMIT_LOW_CONFIDENCE);
   return (
-    <Alert severity="error" sx={{ m: 2, fontSize: "80%" }}>
+    <Alert severity="error" sx={{ m: 2, fontSize: "90%" }}>
       Identification Confidence for this Photo is{" "}
-      <strong>less than {pLimit}</strong>.
-      <br />
+      <strong>under {pLimit}</strong>.
+      <p style={{fontSize: "80%"}}>
       {plantNetResult.confidenceStrImportant}
+      </p>
     </Alert>
   );
 }
