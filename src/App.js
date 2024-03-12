5e9ef73012be5291971ca8adf612ecbc8ad03262
nuuuwan <nuuuwan@gmail.com>
2024-03-12 18:51:20 +0530

    ABeeZee

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ const THEME = createTheme({
     },
   },
   typography: {
-    fontFamily: ["Afacad", "sans-serif"].join(","),
+    fontFamily: ["ABeeZee", "sans-serif"].join(","),
     fontSize: 10,
   },
 });
