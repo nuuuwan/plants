d832fc18283f1ddff283f24966e12c955d224d2e
nuuuwan <nuuuwan@gmail.com>
2024-03-12 18:29:49 +0530

    AlertLowConfidence - Style

--- src/view/molecules/AlertLowConfidence.js
+++ src/view/molecules/AlertLowConfidence.js
@@ -3,12 +3,11 @@ import { PlantNetResult } from "../../nonview/core";
 import { Format } from "../../nonview/base";
 
 export default function AlertLowConfidence({ plantNetResult }) {
-  const pLimit = Format.percent(PlantNetResult.LIMIT_LOW_CONFIDENCE);
+
   return (
-    <Alert severity="error" sx={{ m: 2, fontSize: "90%" }}>
-      Identification Confidence for this Photo is{" "}
-      <strong>under {pLimit}</strong>.
-      <p style={{ fontSize: "80%" }}>{plantNetResult.confidenceStrImportant}</p>
+    <Alert severity="error" sx={{ m: 1, fontSize: "80%" }}>
+      Low indentification confidence!
+      <p style={{ fontSize: "80%",margin: 0, padding: 0 }}>{plantNetResult.confidenceStrImportant}</p>
     </Alert>
   );
 }
