14d9061e4b66d7451fe809bb6c0a827899cf065d
nuuuwan <nuuuwan@gmail.com>
2024-03-16 08:46:18 +0530

    Added SpeciesViewStyle.COLOR; formatting

--- src/view/molecules/SpeciesViewStyle.js
+++ src/view/molecules/SpeciesViewStyle.js
@@ -1,34 +1,42 @@
-let SpeciesViewStyle = {
-  BOX_INFO: {
-    display: "flex",
-    flexDirection: "column",
-    justifyContent: "center",
-    height: "80px",
-    textAlign: "center",
-    fontSize: "90%",
-    p: 0.5,
-    m: 0,
-    background: "white",
-  },
+let SpeciesViewStyle = {};
 
-  FAMILY: {
-    fontSize: "100%",
-    color: "#800",
-  },
-  GENUS: {
-    fontStyle: "italic",
-    fontSize: "120%",
-    color: "#f80",
-  },
+SpeciesViewStyle.COLOR = {
+  family: "#800",
+  genus: "#f80",
+  species: "#082",
+  commonNames: "#444",
+};
 
-  COMMON_NAMES: {
-    fontSize: "80%",
-    color: "#444",
-  },
+SpeciesViewStyle.BOX_INFO = {
+  display: "flex",
+  flexDirection: "column",
+  justifyContent: "center",
+  height: "80px",
+  textAlign: "center",
+  fontSize: "90%",
+  p: 0.5,
+  m: 0,
+  background: "white",
+};
+
+SpeciesViewStyle.FAMILY = {
+  fontSize: "100%",
+  color: SpeciesViewStyle.COLOR.family,
+};
+
+SpeciesViewStyle.GENUS = {
+  fontStyle: "italic",
+  fontSize: "120%",
+  color: SpeciesViewStyle.COLOR.genus,
+};
+
+SpeciesViewStyle.COMMON_NAMES = {
+  fontSize: "80%",
+  color: SpeciesViewStyle.COLOR.commonNames,
 };
 
 SpeciesViewStyle.SPECIES = Object.assign({}, SpeciesViewStyle.GENUS, {
-  color: "#082",
+  color: SpeciesViewStyle.COLOR.species,
 });
 
 export default SpeciesViewStyle;
