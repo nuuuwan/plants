f416a0ca7097fca49303e2ed8fb9f668f5dd8151
nuuuwan <nuuuwan@gmail.com>
2024-03-16 13:52:11 +0530

    const N_DISPLAY_MAX = 5

--- src/nonview/core/HistoryTable.js
+++ src/nonview/core/HistoryTable.js
@@ -1,11 +1,14 @@
 export default class HistoryTable {
   static getDataList(eppIdx, historyEppIDList) {
-    const eppIdx2 = historyEppIDList.reduce(function (eppIdx2, eppId) {
+    const N_DISPLAY_MAX = 5;
+    const filteredHistoryEppIDList =  historyEppIDList.slice(-N_DISPLAY_MAX);
+    const eppIdx2 = filteredHistoryEppIDList.reduce(function (eppIdx2, eppId) {
       const epp = eppIdx[eppId];
 
       eppIdx2[epp.species.name] = epp;
       return eppIdx2;
     }, {});
+
     const eppList = Object.values(eppIdx2);
     const dataList = eppList.map(function (epp) {
       return {
