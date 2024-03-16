af0fb2aecb8f29399ceab567733853519cdaf852
nuuuwan <nuuuwan@gmail.com>
2024-03-16 10:55:11 +0530

    Search Fixes #11

--- src/view/molecules/PhoneBook.js
+++ src/view/molecules/PhoneBook.js
@@ -1,4 +1,12 @@
-import { Box, List, ListItem, ListSubheader, Typography } from "@mui/material";
+import React, { useState } from "react";
+import {
+  Box,
+  List,
+  ListItem,
+  ListSubheader,
+  TextField,
+  Typography,
+} from "@mui/material";
 import PhoneBookStyle from "./PhoneBookStyle.js";
 export default function PhoneBook({
   dataList,
@@ -8,7 +16,24 @@ export default function PhoneBook({
   onClick,
   getStyle,
 }) {
-  const groupToDataList = dataList.reduce(
+  let [filterText, setFilterText] = useState("");
+
+  const onChangeTextFieldSearch = function (event) {
+    const value = event.target.value;
+    setFilterText(value);
+  };
+
+  const filteredDataList = dataList.filter(function (d) {
+    return getLabel(d).toLowerCase().includes(filterText.toLocaleLowerCase());
+  });
+
+  const idList = filteredDataList.map(function (d) {
+    return d.id;
+  });
+  const uniqueIdList = [...new Set(idList)];
+  const nUniqueIdList = uniqueIdList.length;
+
+  const groupToDataList = filteredDataList.reduce(
     function (groupToDataList, d) {
       const group = getLabel(d).substring(0, 1).toUpperCase();
       const isAlpha = /^[a-z]+$/i.test(group);
@@ -29,41 +54,51 @@ export default function PhoneBook({
   );
 
   return (
-    <List sx={PhoneBookStyle}>
-      {sortedGroups.map(function (group) {
-        const dataList = groupToDataList[group];
-        const sortedDataList = dataList.sort(function (a, b) {
-          return getLabel(a).localeCompare(getLabel(b));
-        });
-        const nDataList = sortedDataList.length;
-        if (nDataList === 0) {
-          return null;
-        }
-        const key = `group-${group}`;
-        return (
-          <Box key={key}>
-            <ListSubheader>
-              <Typography variant="h5">{group}</Typography>
-            </ListSubheader>
-            {sortedDataList.map(function (d, i) {
-              const onClickInner = function () {
-                return onClick(d);
-              };
-              const key = "item-" + i;
-              const label = getLabel(d);
-              const n = getN(d);
-              const labelDisplay = label + (n > 1 ? ` (${n})` : "");
-              return (
-                <ListItem key={key} sx={{ m: 0.5, marginLeft: 3, p: 0 }}>
-                  <Typography sx={getStyle(d)} onClick={onClickInner}>
-                    {labelDisplay}
-                  </Typography>
-                </ListItem>
-              );
-            })}
-          </Box>
-        );
-      })}
-    </List>
+    <Box sx={PhoneBookStyle.BOX}>
+      <TextField
+        placeholder="Search"
+        sx={PhoneBookStyle.TEXT_FIELD_SEARCH}
+        onChange={onChangeTextFieldSearch}
+      />
+      <Typography variant="body2" sx={PhoneBookStyle.LABEL_N_RESULTS}>
+        {nUniqueIdList} species found.
+      </Typography>
+      <List>
+        {sortedGroups.map(function (group) {
+          const dataList = groupToDataList[group];
+          const sortedDataList = dataList.sort(function (a, b) {
+            return getLabel(a).localeCompare(getLabel(b));
+          });
+          const nDataList = sortedDataList.length;
+          if (nDataList === 0) {
+            return null;
+          }
+          const key = `group-${group}`;
+          return (
+            <Box key={key}>
+              <ListSubheader>
+                <Typography variant="h5">{group}</Typography>
+              </ListSubheader>
+              {sortedDataList.map(function (d, i) {
+                const onClickInner = function () {
+                  return onClick(d);
+                };
+                const key = "item-" + i;
+                const label = getLabel(d);
+                const n = getN(d);
+                const labelDisplay = label + (n > 1 ? ` (${n})` : "");
+                return (
+                  <ListItem key={key} sx={{ m: 0.5, marginLeft: 3, p: 0 }}>
+                    <Typography sx={getStyle(d)} onClick={onClickInner}>
+                      {labelDisplay}
+                    </Typography>
+                  </ListItem>
+                );
+              })}
+            </Box>
+          );
+        })}
+      </List>
+    </Box>
   );
 }
--- src/view/molecules/PhoneBookStyle.js
+++ src/view/molecules/PhoneBookStyle.js
@@ -1,4 +1,18 @@
 let PhoneBookStyle = {};
 PhoneBookStyle.width = 240;
 
+PhoneBookStyle.BOX = {
+  width: PhoneBookStyle.width,
+};
+
+PhoneBookStyle.TEXT_FIELD_SEARCH = {
+  width: "100%",
+  margin: 1,
+  padding: 0.5,
+};
+
+PhoneBookStyle.LABEL_N_RESULTS = {
+  marginLeft: 2,
+};
+
 export default PhoneBookStyle;
