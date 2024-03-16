a935d7647e7389f2126e384c875c5db5c56fd485
nuuuwan <nuuuwan@gmail.com>
2024-03-16 13:41:43 +0530

    DEFAULT_ZOOM = 17

--- src/nonview/base/GeoData.js
+++ src/nonview/base/GeoData.js
@@ -3,5 +3,5 @@ export default class GeoData {
   static DEFAULT_CENTER = [6.9127942713613715, 79.86130971981015];
   // 1 <= Zoom <= 18
 
-  static DEFAULT_ZOOM = 18;
+  static DEFAULT_ZOOM = 17;
 }
