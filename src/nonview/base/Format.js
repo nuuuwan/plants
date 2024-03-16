e13189a486bbaff22b848b1042f123b0bd1cbfd4
nuuuwan <nuuuwan@gmail.com>
2024-03-16 09:00:05 +0530

    Added dateStr, timeOnlyStr, dateTimeStr

--- src/nonview/base/Format.js
+++ src/nonview/base/Format.js
@@ -2,4 +2,29 @@ export default class Format {
   static percent(x) {
     return (x * 100).toFixed(0) + "%";
   }
+
+  static dateStr(x) {
+    let dateOptions = {
+      year: "numeric",
+      month: "short",
+      day: "numeric",
+      weekday: "short",
+    };
+
+    return x.toLocaleDateString("en-US", dateOptions);
+  }
+
+  static timeOnlyStr(x) {
+    let timeOptions = {
+      hour: "numeric",
+      minute: "numeric",
+      second: "numeric",
+    };
+
+    return x.toLocaleTimeString("en-US", timeOptions);
+  }
+
+  static dateTimeStr(x) {
+    return `${Format.dateStr(x)} · ${Format.timeOnlyStr(x)}`;
+  }
 }
