d888880ffaf4e7bcfabc2ec4b6472186bb5177bb
nuuuwan <nuuuwan@gmail.com>
2024-03-16 14:07:58 +0530

    Increase button size - part 1

--- src/view/atoms/ButtonSettingsStyle.js
+++ src/view/atoms/ButtonSettingsStyle.js
@@ -2,6 +2,10 @@ let ButtonSettingsStyle = {
   position: "absolute",
   bottom: "8%",
   right: "5%",
+  
   backgroundColor: "white",
+  
+  height: 60,
+  width: 60,
 };
 export default ButtonSettingsStyle;
