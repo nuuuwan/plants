471df64cfb2c43b51f10d91710b25c6f8e8a6721
nuuuwan <nuuuwan@gmail.com>
2024-03-21 15:21:58 +0530

    changed zoom

--- src/nonview/base/GeoData.js
+++ src/nonview/base/GeoData.js
@@ -1,6 +1,6 @@
 export default class GeoData {
   // Central Fountain, Viharamahadevi Park, Colombo, Sri Lanka
-  static DEFAULT_CENTER = [6.9127942713613715, 79.86130971981015];
+  static DEFAULT_CENTER = [6.913451667934583, 79.86169655008696];
   // 1 <= Zoom <= 18
 
   static DEFAULT_ZOOM = 18;
